290297ba4d2f1f7fa8dc30c195faa94abdabb39b
Benjamin O'Connell-Armand <benjamin.oconnell@polymtl.ca>
2015-07-03 14:22:23 -0400

    Moved argument handling outside of the function.

--- commotion-router-testbench.py
+++ commotion-router-testbench.py
@@ -5,9 +5,41 @@ import getopt
 import routingTest.testRouting
 
 
-def main(argv):
+def test_firmware_image(router_list_file):
+    ## We will not move the files (try to do it through jenkins or something?)
+
+    # Copy new build to the tftp server (overwrite older builds)
+    # openwrt-ar71xx-generic-hornet-ub-x2-kernel.bin
+    # os.system("cp " + kernelPath + " " + tftpPath + "kernel.bin")
+    # openwrt-ar71xx-generic-hornet-ub-x2-rootfs-squashfs.bin
+    # os.system("cp " + rootfsPath + " " + tftpPath + "rootfs.bin")
+    routers = []
+
+    # Read from input file the serial ports (routers) to communicate with
+    with open(router_list_file, 'r') as f:
+        for line in f:
+            line = line.rstrip('\n')
+
+            if line.startswith('/dev/tty'):
+                # Flash new build on routers
+                routers.append(line)
+
+    for i, r in enumerate(routers):
+        ip = '192.168.2.1{0}'.format(i)
+
+        if flash.flash(r, ip):
+            routers.append(r)
+        else:
+            # For now, if a flash fails, you will need to reboot that router into uboot manually and run the script
+            # on that router again (or flash manually if you prefer).
+            print("Flashing failed on router with port " + r)
+
+            # Ping test each router to make sure mesh is up
+
+
+if __name__ == "__main__":
     try:
-        opts, args = getopt.getopt(argv, "hi:", "ifile=")
+        opts, args = getopt.getopt(sys.argv[1], "hi:", "ifile=")
     except getopt.GetoptError:
         print 'commotion-router-testbench.py -i <inputfile>'
         sys.exit(2)
@@ -22,38 +54,5 @@ def main(argv):
             sys.exit()
         elif opt in ("-i", "--ifile"):
             input_file = arg
-
-    ## We will not move the files (try to do it through jenkins or something?)
-
-    # Copy new build to the tftp server (overwrite older builds)
-    # openwrt-ar71xx-generic-hornet-ub-x2-kernel.bin
-    # os.system("cp " + kernelPath + " " + tftpPath + "kernel.bin")
-    # openwrt-ar71xx-generic-hornet-ub-x2-rootfs-squashfs.bin
-    # os.system("cp " + rootfsPath + " " + tftpPath + "rootfs.bin")
-    routers = []
-
-    # Read from input file the serial ports (routers) to communicate with
-    with open(input_file, 'r') as f:
-        for line in f:
-            line = line.rstrip('\n')
-
-            if line.startswith('/dev/tty'):
-                # Flash new build on routers
-                routers.append(line)
-
-    for i, r in enumerate(routers):
-        ip = '192.168.2.1{0}'.format(i)
-
-        if flash.flash(r, ip):
-            routers.append(line)
-        else:
-            # For now, if a flash fails, you will need to reboot that router into uboot manually and run the script
-            # on that router again (or flash manually if you prefer).
-            print("Flashing failed on router with port " + r)
-
-            # Ping test each router to make sure mesh is up
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+    test_firmware_image(input_file)
     quit()
